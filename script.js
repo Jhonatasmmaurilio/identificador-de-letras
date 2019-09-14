0286168aa483f1e003227826336c010122e06d1b
Jhonatas <jhonatas.maurilio@hotmail.com>
2019-09-14 18:55:05 -0300

    Inicio
    - Adicionado arquivo iniciais

---
+++ script.js
@@ -0,0 +1,30 @@
+var pesosX,
+		pesosZ,
+		entradas,
+		saidas;
+
+pesosX = new Array(
+				[0.1, 0.1, 0],
+				[0.3, 0.1, 0.9],
+				[0.5, 0.4, 1]
+);
+
+pesosZ = new Array(
+	[0.6, 0.3],
+	[0.5, 0.8],
+	[0.1, 0.3]
+);
+
+entradas = new Array(
+	[1, 1, 0],
+	[1, 0, 0],
+	[0, 0, 1]
+);
+
+saidas = new Array(1,1)
+
+console.log(entradas[0]);
+
+function Passo03() {
+	
+}
